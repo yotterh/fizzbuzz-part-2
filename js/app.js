fc34816803a1dd6143cddbc7a0bd793650838d00
yotter <hollyyotter11@gmail.com>
2016-03-13 23:34:06 -0400

    final project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-	$('.go').click(function() {
-		for (var start = 1; start < 101; start++) {
+	$('form').submit(function(e) {
+		e.preventDefault();
+		var input = $('input[name="number"]').val();
+			if(isNaN(input)){
+				alert("Please enter a number only!");
+			}
+			else if(input % 1 !== 0) {
+				alert("Please enter a whole number only!");
+			}
+			else if(input < 1 ) {
+				alert("Please enter a number between 1-100 only!");
+			}
+			else if(input > 100 ) {
+				alert("Please enter a number between 1-100 only!");
+			}
+			else 
+		for (var start = 1; start < input; start++) {
 			if (start % 3 === 0 && start % 5 ===0) {
 				$('ul').append('Fizz Buzz')
 			}
